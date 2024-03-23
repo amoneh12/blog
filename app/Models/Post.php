7be35063c30bc44d83aedc5a91035b7e895685e6
amoneh12 <amanisabrah7@gmail.com>
2024-03-23 14:03:22 +0300

    modify models files

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,11 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
 use Astrotomic\Translatable\Translatable;
 
-class Post extends Model implements PostTranslation
+class Post extends Model implements TranslatableContract
 {
     use Translatable;
     use HasFactory;
 
-    public $translatedAttributes = ['title', 'content'];
+    public $translatedAttributes = ['title', 'content','smallDes'];
     protected $fillable = ['id', 'image', 'category_id', 'created_at', 'updated_at', 'deleted_at'];
     }
